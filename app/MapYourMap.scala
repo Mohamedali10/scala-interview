3890653a56d5056146774c3fa2b66339b2527f6d
Mohamedali10 <bouine.mohamedali@gmail.com>
2021-03-17 23:21:27 +0100

    Update MapYourMap.scala
    code enhancement

--- app/MapYourMap.scala
+++ app/MapYourMap.scala
@@ -10,8 +10,7 @@ object MapYourMap {
   val devNames = Map("dev1" -> "Pierre", "dev2" -> "Remy", "dev3" -> "Noe", "dev4" -> "Alexandre")
   val devDepartments = Map("dev1" -> "analytics", "dev2" -> "frontend", "dev3" -> "api", "dev4" -> "frontend")
 
-  val namesInDepartments:Map[String, List[String]] = devDepartments.groupBy { case (de, dp) => dp }
-    .map { case (ke, maps) => ke -> devNames.filter {
-      case (kev, v) => maps.keySet.contains(kev)
-    }.values.toList}
-}
+  val namesInDepartments:Map[String, List[String]] = devDepartments.groupBy(_._2)
+    .map { case (department, devDepmnts) => 
+    department -> devDepmnts.keySet.map(devNames).toList
+    }
